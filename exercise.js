6ed4485db150e0cfeef9a6996c4311f95afaf6c5
Massimo <rabuffo.massimo@gmail.com>
2023-08-16 21:23:12 +0200

    exercise done

---
+++ exercise.js
@@ -0,0 +1,7 @@
+const order = {};
+
+// if (order && order.customer && order.customer.address && !order.customer.address.city) {
+//   console.log('City is required');
+// }
+
+ console.log(order.costumer?.address?.city?.());
